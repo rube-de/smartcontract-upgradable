7825758b4b18df2e81976156f4b67e9346e6283f
rube-de <rube@ruf2.de>
2021-05-02 19:15:49 +0200

    update proxy and test with new updated contract

--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -1,4 +1,5 @@
 const Cats = artifacts.require("Cats");
+const CatsUpdated = artifacts.require("CatsUpdated");
 const Proxy = artifacts.require("Proxy");
 
 module.exports = async function(deployer, network, accounts){
@@ -14,5 +15,25 @@ module.exports = async function(deployer, network, accounts){
 
     //test
     var nrOfCats = await proxyCats.getNumberOfCats();
-    console.log(nrOfCats.toNumber());
+    console.log("Before update: " + nrOfCats.toNumber());
+
+    //Deploy new version of Cats
+    const catsUpdated = await CatsUpdated.new();
+    proxy.upgrade(catsUpdated.address);
+
+    //update truffle view, to access all new CatsUpdated functions
+    proxyCats = await CatsUpdated.at(proxy.address);
+    //Initialize proxy state
+    proxyCats.initialize(accounts[0]);
+
+    //Check if storage remained
+     nrOfCats = await proxyCats.getNumberOfCats();
+    console.log("After update: " + nrOfCats.toNumber());
+
+    //Set number of the cats through proxy with the new updated contract
+    await proxyCats.setNumberOfCats(30);
+
+    //Check if storage changed
+    nrOfCats = await proxyCats.getNumberOfCats();
+    console.log("After set in update contract: " + nrOfCats.toNumber());
 }
