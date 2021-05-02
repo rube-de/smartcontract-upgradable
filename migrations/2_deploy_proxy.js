a74731551306e826a4c4cc6411e7cf44cb5e6a3c
rube-de <rube@ruf2.de>
2021-05-02 18:01:09 +0200

    add comments

--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -2,6 +2,17 @@ const Cats = artifacts.require("Cats");
 const Proxy = artifacts.require("Proxy");
 
 module.exports = async function(deployer, network, accounts){
+    //Deploy Contracts
     const cats = await Cats.new();
     const proxy = await Proxy.new(cats.address);
+
+    //Create Proxy Cat for truffle
+    var proxyCats = await Cats.at(proxy.address);
+
+    //Set number of the cats through proxy
+    await proxyCats.setNumberOfCats(10);
+
+    //test
+    var nrOfCats = await proxyCats.getNumberOfCats();
+    console.log(nrOfCats.toNumber());
 }
